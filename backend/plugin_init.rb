d6109f3729b59f83608b470f0054fbf7379d0f37
James Bullen <jambun@gmail.com>
2018-11-21 22:38:53 +1100

    Register Function and Mandate models for history

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -25,3 +25,10 @@ Permission.define("delete_mandate_record",
                   "The ability to create and modify function records",
                   :implied_by => 'manage_mandate_record',
                   :level => "global")
+
+begin
+  History.register_model(Mandate)
+  History.register_model(Function)
+rescue NameError
+  Log.info("Unable to register Mandate and Function for history. Please install the as_history plugin")
+end
