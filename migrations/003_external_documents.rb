2b4940c2de5af6b76dc132e346e3cd55b75febe1
Sean Anderson <sean.anderson@gaiaresources.com.au>
2018-11-23 10:21:35 +0800

    * Start_date/end_date validation error message * Unit tests (WIP) * Rubocop cleanup * Added missing functions_agencies mixin

--- migrations/003_external_documents.rb
+++ migrations/003_external_documents.rb
@@ -1,10 +1,10 @@
 require 'db/migrations/utils'
 
 Sequel.migration do
-  records_supporting_external_documents = [:function]
+  records_supporting_ext_docs = [:function]
 
   up do
-    records_supporting_external_documents.each do |record|
+    records_supporting_ext_docs.each do |record|
       alter_table(:external_document) do
         add_column("#{record}_id".intern, :integer, null: true)
         add_foreign_key(["#{record}_id".intern], record.intern, key: :id)
