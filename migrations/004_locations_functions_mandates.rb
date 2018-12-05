0b88b6e1ba6ca4793e4a02a03102089f1176a60c
Sean Anderson <sean.anderson@gaiaresources.com.au>
2018-12-05 18:04:53 +0800

    * Tests

--- migrations/004_locations_functions_mandates.rb
+++ migrations/004_locations_functions_mandates.rb
@@ -10,11 +10,12 @@ Sequel.migration do
       add_unique_constraint(:title, :name => "mandate_name_uniq")
     end
 
-    create_table(:top_function_rlshp) do
+    create_table(:location_rlshp) do
       primary_key :id
 
-      Integer :top_container_id
-      Integer :function_id
+      Integer :location_id
+      Integer :function_id, null: true
+      Integer :mandate_id, null: true
       Integer :aspace_relationship_position
 
       Integer :suppressed, :null => false, :default => 0
@@ -25,7 +26,7 @@ Sequel.migration do
     alter_table(:location_rlshp) do
       add_foreign_key([:location_id], :location, :key => :id)
       add_foreign_key([:function_id], :function, :key => :id)
-      add_foreign_key([:mandate_id], :mandate, :key => :id)
+      add_foreign_key([:mandate_id], :function, :key => :id)
     end
   end
 end
