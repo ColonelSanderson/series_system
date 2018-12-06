c428a138dc75d7a0deedd8f44fb9823b99b33230
Payten Giles <payten.giles@gmail.com>
2018-12-06 15:01:25 +1100

    Fix missing functions field on agent_corporate_entity schema ext

--- schemas/agent_corporate_entity_ext.rb
+++ schemas/agent_corporate_entity_ext.rb
@@ -18,4 +18,23 @@
       }
     }
   },
+  "functions" => {
+    "type" => "array",
+    "items" => {
+      "type" => "object",
+      "subtype" => "ref",
+      "properties" => {
+        "ref" => {
+          "type" => "JSONModel(:function) uri",
+          "ifmissing" => "error"
+        },
+        "start_date" => {"type" => "date"},
+        "end_date" => {"type" => "date"},
+        "_resolved" => {
+          "type" => "object",
+          "readonly" => "true"
+        }
+      }
+    }
+  },
 }
