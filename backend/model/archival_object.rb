31eb95645510e8c9064d1d68bd856eaf94ef9fa3
James Bullen <jambun@gmail.com>
2019-02-11 15:51:03 +1100

    Change the ref_id auto-generator to use a sequence

--- backend/model/archival_object.rb
+++ backend/model/archival_object.rb
@@ -1,2 +1,10 @@
 ArchivalObject.include(FunctionsArchivalRecords)
 ArchivalObject.include(MandatesArchivalRecords)
+
+class ArchivalObject
+
+  auto_generate :property => :ref_id,
+                :generator => proc { |json| Sequence.get('SERIES_SYSTEM_REF_ID').to_s },
+                :only_on_create => true
+
+end
