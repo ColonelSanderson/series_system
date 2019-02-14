1ba45083d452355d25b0bcb359bde554c2555d4c
James Bullen <jambun@gmail.com>
2019-02-14 12:41:28 +1100

    User defined enums for security level and sensitivity label

---
+++ migrations/006_populate_ud_enums_for_security_and_sensitivity.rb
@@ -0,0 +1,28 @@
+require 'db/migrations/utils'
+
+Sequel.migration do
+
+  up do
+    enum_1_id = self[:enumeration].filter(:name => 'user_defined_enum_1').get(:id)
+
+    self[:enumeration_value].filter(:enumeration_id => enum_1_id).delete
+
+    ['official', 'sensitive', 'protected'].each_with_index do |val, ix|
+      self[:enumeration_value].insert({:enumeration_id => enum_1_id, :value => val, :position => ix})
+    end
+
+# values not yet provided
+#     enum_2_id = self[:enumeration].filter(:name => 'user_defined_enum_2').get(:id)
+
+#    self[:enumeration_value].filter(:enumeration_id => enum_2_id).delete
+
+#     [].each_with_index do |val, ix|
+#       self[:enumeration_value].insert({:enumeration_id => enum_2_id, :value => val, :position => ix})
+#     end
+
+  end
+
+  down do
+  end
+
+end
