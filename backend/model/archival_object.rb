1348a33240111874977131b263bb9d90b51925bc
James Bullen <jambun@gmail.com>
2019-03-07 14:02:02 +1100

    removed auto generators - this is now handled in as_runcorn

--- backend/model/archival_object.rb
+++
@@ -1,7 +0,0 @@
-class ArchivalObject
-
-  auto_generate :property => :ref_id,
-                :generator => proc { |json| Sequence.get('SERIES_SYSTEM_REF_ID').to_s },
-                :only_on_create => true
-
-end
