5477f0e653e5aa92ea0dc479761eb9f2a9c84126
jambun <jambun@gmail.com>
2019-03-13 11:39:54 +1100

    tidied

--- frontend/assets/series_system_relationships.crud.js
+++ frontend/assets/series_system_relationships.crud.js
@@ -29,11 +29,8 @@ $(function() {
         }
     });
 
-    $(document).on('change', 'section[id^=series_system_] input.linker', function () {
-	    console.log('weeeeeeeeeee');
-    });
 
-    dateBounds = function(date, startDate, endDate) {
+    var dateBounds = function(date, startDate, endDate) {
 	if (date['label'] == 'existence') {
 	    if (date.hasOwnProperty('begin')) {
 		if (!startDate || startDate > date['begin']) {
@@ -50,77 +47,84 @@ $(function() {
     };
 
 
+    var calculateCommonDates = function(linker, init) {
+	var $linker = $(linker);
+
+	if (!init && !$linker.parents('.subrecord-form-container').find('.token-input-token:first').is('[id]')) {
+	    // linker is empty, so set placeholder and get out
+	    var $common = $linker.parents('.subrecord-form-container').find('.series-system-relationship-common-dates');
+	    $common.html($common.data('placeholder'));
+	    return;
+	}
+
+	obj = $linker.data('selected');
+
+	var startDate = false;
+	var endDate = false;
+
+	if (obj.hasOwnProperty('date')) {
+	    var newDates = dateBounds(obj['date'], startDate, endDate);
+	    startDate = newDates[0];
+	    endDate = newDates[1];
+	}
+
+	if (obj.hasOwnProperty('dates_of_existence')) {
+	    $(obj['dates_of_existence']).each(function() {
+		var newDates = dateBounds(this, startDate, endDate);
+		startDate = newDates[0];
+		endDate = newDates[1];
+	    });
+	}
+
+	$datesSection = $('section[id$=_dates_of_existence]');
+
+	var myStartDate = false;
+	var myEndDate = false;
+
+	$datesSection.find('input[id$=_begin_]').each(function() {
+	    if (!myStartDate || $(this).val() < myStartDate) {
+		myStartDate = $(this).val();
+	    }
+	});
+
+	$datesSection.find('input[id$=_end_]').each(function() {
+	    if (!myEndDate || $(this).val() > myEndDate) {
+		myEndDate = $(this).val();
+	    }
+	});
+
+	if (!startDate || (myStartDate && myStartDate.padEnd(10, '0') > startDate.padEnd(10, '0'))) {
+	    startDate = myStartDate;
+	}
+
+	if (!endDate || (myEndDate && myEndDate.padEnd(10, '9') < endDate.padEnd(10, '9'))) {
+	    endDate = myEndDate;
+	}
+
+	var msg = '';
+	if (startDate) {
+	    msg += startDate + ' -- ';
+	} else {
+	    msg += 'up to ';
+	}
+	if (endDate) {
+	    msg += endDate;
+	} else {
+	    msg += 'present';
+	}
+
+	$linker.parents('.subrecord-form-container').find('.series-system-relationship-common-dates').html(msg);
+    };
+
+
+    $(document).on('change', 'section[id^=series_system_] input.linker', function () {
+	calculateCommonDates(this);
+    });
+
+
     $(document).on('loadedrecordform.aspace', function () {
         $('section[id^=series_system_] input.linker.initialised').each(function() {
-		$linker = $(this);
-		obj = $linker.data('selected');
-
-		var startDate = false;
-		var endDate = false;
-
-		if (obj.hasOwnProperty('date')) {
-		    newDates = dateBounds(obj['date'], startDate, endDate);
-		    startDate = newDates[0];
-		    endDate = newDates[1];
-		}
-
-		if (obj.hasOwnProperty('dates_of_existence')) {
-		    $(obj['dates_of_existence']).each(function() {
-			    newDates = dateBounds(this, startDate, endDate);
-			    startDate = newDates[0];
-			    endDate = newDates[1];
-			});
-		}
-
-		console.log('start:  ' + startDate);
-		console.log('end:    ' + endDate);
-
-		$datesSection = $('section[id$=_dates_of_existence]');
-
-		console.log($datesSection);
-
-		var myStartDate = false;
-		var myEndDate = false;
-
-		$datesSection.find('input[id$=_begin_]').each(function() {
-			if (!myStartDate || $(this).val() < myStartDate) {
-			    myStartDate = $(this).val();
-			}
-		    });
-
-		$datesSection.find('input[id$=_end_]').each(function() {
-			if (!myEndDate || $(this).val() > myEndDate) {
-			    myEndDate = $(this).val();
-			}
-		    });
-
-		console.log('my start:  ' + myStartDate);
-		console.log('my end:    ' + myEndDate);
-
-		if (!startDate || (myStartDate && myStartDate.padEnd(10, '0') > startDate.padEnd(10, '0'))) {
-		    startDate = myStartDate;
-		}
-
-		if (!endDate || (myEndDate && myEndDate.padEnd(10, '9') < endDate.padEnd(10, '9'))) {
-		    endDate = myEndDate;
-		}
-
-		var label = '<label class="control-label col-sm-2">Common dates</label>';
-		var msg = '';
-		if (startDate) {
-		    msg += startDate + ' -- ';
-		} else {
-		    msg += 'up to ';
-		}
-		if (endDate) {
-		    msg += endDate;
-		} else {
-		    msg += 'present';
-		}
-		msg = '<div class="col-sm-9"><div class="label-only">' + msg + '</div></div>';
-
-		$('<div class="form-group">' + label + msg + '</div>').insertAfter($linker.parents('.form-group:first'));
-
+	    calculateCommonDates(this, true);
 	});
     });
 });
