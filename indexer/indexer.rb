0eb15ef4a09447fbea471289315b6fcbf1ce68b5
Mark Triggs <mark@dishevelled.net>
2019-05-15 10:48:57 +1000

    Move MAP-specific indexing into as_cartography

--- indexer/indexer.rb
+++ indexer/indexer.rb
@@ -75,20 +75,6 @@ class IndexerCommon
         end
       end
     end
-
-    indexer.add_document_prepare_hook do |doc, record|
-      if record['record'].has_key?('responsible_agency')
-        doc['responsible_agency_u_sstr'] = record['record']['responsible_agency']['ref']
-      end
-
-      if record['record'].has_key?('recent_responsible_agencies')
-        doc['recent_responsible_agencies_u_sstr'] = record['record']['recent_responsible_agencies'].map{|r| r['ref']}
-      end
-
-      if record['record'].has_key?('other_responsible_agencies')
-        doc['other_responsible_agencies_u_sstr'] = record['record']['other_responsible_agencies'].map{|r| r['ref']}
-      end
-    end
   end
 
 
