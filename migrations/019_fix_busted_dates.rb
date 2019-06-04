50dd31a8fa6cec8046672296cf769bfb6006cb39
Payten Giles <payten.giles@gmail.com>
2019-06-04 10:42:41 +1000

    Only run 019_fix_busted_dates migration for mysql (as breaks on derby/unit tests)

--- migrations/019_fix_busted_dates.rb
+++ migrations/019_fix_busted_dates.rb
@@ -12,26 +12,26 @@ Sequel.migration do
       end
     }
 
+    if self.database_type == :mysql
+      self.transaction do
+        self["select id from date " +
+             " where (begin is not null AND begin not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')" +
+             "  OR (end is not null AND end not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')"].each do |row|
+          busted_row = self[:date][:id => row[:id]]
 
-    self.transaction do
-      self["select id from date " +
-           " where (begin is not null AND begin not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')" +
-           "  OR (end is not null AND end not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')"].each do |row|
-        busted_row = self[:date][:id => row[:id]]
+          self[:date].filter(:id => row[:id]).update(:begin => fix_date.call(busted_row[:begin]),
+                                                     :end => fix_date.call(busted_row[:end]))
+        end
 
-        self[:date].filter(:id => row[:id]).update(:begin => fix_date.call(busted_row[:begin]),
-                                                   :end => fix_date.call(busted_row[:end]))
+        self["select id from series_system_rlshp " +
+             " where (start_date is not null AND start_date not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')" +
+             "  OR (end_date is not null AND end_date not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')"].each do |row|
+          busted_row = self[:series_system_rlshp][:id => row[:id]]
+
+          self[:series_system_rlshp].filter(:id => row[:id]).update(:start_date => fix_date.call(busted_row[:start_date]),
+                                                                    :end_date => fix_date.call(busted_row[:end_date]))
+        end
       end
-
-      self["select id from series_system_rlshp " +
-           " where (start_date is not null AND start_date not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')" +
-           "  OR (end_date is not null AND end_date not regexp '^[0-9]{4}(-[0-9]{2})?(-[0-9]{2})?$')"].each do |row|
-        busted_row = self[:series_system_rlshp][:id => row[:id]]
-
-        self[:series_system_rlshp].filter(:id => row[:id]).update(:start_date => fix_date.call(busted_row[:start_date]),
-                                                                  :end_date => fix_date.call(busted_row[:end_date]))
-      end
-
     end
   end
 
